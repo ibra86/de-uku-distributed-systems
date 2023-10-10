222c0d811a57e45ed4a5402a37aa89dc19ae859b
Anton Ibragimov <ibrahimov.ag@gmail.com>
2023-10-10 22:18:50 +0300

    add test master post

---
+++ test/__init__.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ['SECONDARY_01_URL'] = 'http://mock'
---
+++ test/test_app_00_master.py
@@ -0,0 +1,23 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from app_00_master import app
+from app_00_master import messages
+
+
+@pytest.fixture
+def client():
+    yield TestClient(app)
+
+
+def test_add_message(mocker, client):
+    mocker.patch('service.MessageService.send_message', autospec=True, return_value='msg_sent')
+
+    response = client.post(
+        '/',
+        json={'name': 'msg'}
+    )
+    assert response.status_code == 200
+    assert response.json()['name'] == 'msg'
+    assert response.json()['timestamp']
+    assert messages == ['msg_sent']
